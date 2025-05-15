618feae9f95692e8e83ac151b41dd3f1437b969d
spadaritucks <thiago.spadari02@gmail.com>
2025-05-15 14:42:13 -0300

    Adiciona cabeçalhos CORS na configuração do Next.js, permitindo requisições de diferentes origens e métodos, melhorando a interoperabilidade da aplicação.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,27 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
