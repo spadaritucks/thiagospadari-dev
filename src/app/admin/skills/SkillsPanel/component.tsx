f6a780b84eecf73648d83533ae37b9f544282a6c
spadaritucks <thiago.spadari02@gmail.com>
2025-05-15 14:12:25 -0300

    Refatora a página de habilidades, movendo a lógica para um novo componente SkillsPanel e implementando um carregamento mais eficiente com Suspense. Adiciona novos componentes para gerenciamento de habilidades, incluindo formulários e esqueleto de carregamento, melhorando a organização e a experiência do usuário.

---
+++ src/app/admin/skills/SkillsPanel/component.tsx
@@ -0,0 +1,133 @@
+'use client'
+import { DeleteOptions, SkillsContent, SkillsHeader, SkillsManagerContent, SkillsTable } from "./styles";
+import { useModal } from "@/context/ModalContext";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { api } from "@/lib/axios";
+import { Pagination } from "@/components/pagination/component";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { z } from "zod";
+import { Button } from "@/components/button/component";
+import { toast } from "sonner";
+import { GetPaginatedSkillsResponse } from "@/RequestTypes/GetPaginatedSkillsResponse";
+import { SkillForm } from "./SkillForm/component";
+import { SkillsSkeleton } from "./SkillsSkeleton/component";
+
+
+
+export default function SkillsPanel() {
+
+  const { openModal, hideModal } = useModal()
+
+
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const router = useRouter()
+  const queryClient = useQueryClient()
+
+  const pageIndex = z.coerce
+    .number()
+    .transform(page => page - 1)
+    .parse(searchParams.get('pageIndex') ?? "1")
+
+
+  const { data, isLoading } = useQuery({
+    queryFn: async () => {
+      const response = await api.get<GetPaginatedSkillsResponse>(`/skills/get-skills`, {
+        params: {
+          pageIndex,
+          pageSize: 10,
+          all : "false"
+        }
+      })
+      return response.data
+    },
+    queryKey: ['skills', pageIndex]
+  })
+
+  function HandlePaginate(newPageIndex: number) {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set('pageIndex', (newPageIndex + 1).toString())
+
+    router.replace(`${pathname}?${params.toString()}`);
+
+  }
+
+  function HandleConfirmDeleteSkill(id: string) {
+    openModal("Tem certeza que deseja excluir?",
+      <DeleteOptions>
+        <Button variant="destructive" onClick={() => HandleDeleteSkill(id)}>Sim</Button>
+        <Button variant="secondary" onClick={hideModal} >Não</Button>
+      </DeleteOptions>
+    )
+  }
+
+  async function HandleDeleteSkill(id: string) {
+    try {
+      const response = await api.delete(`/skills/delete-skills/${id}`)
+      
+      toast.success(response.data.message)
+
+      queryClient.invalidateQueries({
+        queryKey: ["skills"],
+        exact : false
+      })
+      
+      hideModal()
+    } catch (error: any) {
+       toast.error(error.response.data.message)
+    }
+  }
+
+
+
+
+  return (
+  
+      <SkillsContent>
+        <SkillsHeader>
+          <h2>Skills</h2>
+          <Button variant="primary" onClick={() => {
+            openModal("New Skill",
+              <SkillForm />
+            )
+          }}>
+            New Skill
+          </Button>
+        </SkillsHeader>
+        <SkillsManagerContent>
+          <SkillsTable>
+            <thead>
+              <tr>
+                <th>Identifier</th>
+                <th>Icon</th>
+                <th>Name</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {isLoading && <SkillsSkeleton/>}
+              {data?.skills.map((skill, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{skill.id}</td>
+                    <td><img src={skill.image} width={40} height={40} alt="" /></td>
+                    <td>{skill.name}</td>
+                    <td>
+                      <Button variant="destructive" onClick={() => HandleConfirmDeleteSkill(skill.id)}>Excluir</Button>
+                    </td>
+                  </tr>
+                )
+              }) }
+            </tbody>
+          </SkillsTable>
+          {data?.meta && <Pagination
+            onPageChange={HandlePaginate}
+            pageIndex={pageIndex}
+            totalCount={data.meta.totalCount}
+            perPage={data.meta.perPage}
+          />}
+        </SkillsManagerContent>
+      </SkillsContent>
+
+  )
+}
--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,138 +1,16 @@
-'use client'
+
 import { AuthBar } from "@/components/authbar/component";
-import { DeleteOptions, UsersContent, UsersHeader, UsersManagerContent, UsersTable } from "./styles";
-import { useModal } from "@/context/ModalContext";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { UserForm } from "./UsersForm/component";
-import { api } from "@/lib/axios";
-import { Pagination } from "@/components/pagination/component";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { z } from "zod";
-import { Button } from "@/components/button/component";
-import { toast } from "sonner";
-import { GetPaginatedUsersResponse } from "@/RequestTypes/GetPaginatedUsers";
-import { UsersSkeleton } from "./UsersSkeleton/component";
-
-
-
-
-
+import { Suspense } from "react";
+import UsersPanel from "./UsersPanel/component";
 
 export default function UsersPage() {
-
-  const { openModal, hideModal } = useModal()
-
-
-  const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const router = useRouter()
-  const queryClient = useQueryClient()
-
-  const pageIndex = z.coerce
-    .number()
-    .transform(page => page - 1)
-    .parse(searchParams.get('pageIndex') ?? "1")
-
-
-  const { data, isLoading } = useQuery({
-    queryFn: async () => {
-      const response = await api.get<GetPaginatedUsersResponse>(`/users/get-users`, {
-        params: {
-          pageIndex,
-          pageSize: 10,
-          all: "false"
-        }
-      })
-      return response.data
-    },
-    queryKey: ['users', pageIndex]
-  })
-
-  function HandlePaginate(newPageIndex: number) {
-    const params = new URLSearchParams(searchParams.toString())
-    params.set('pageIndex', (newPageIndex + 1).toString())
-
-    router.replace(`${pathname}?${params.toString()}`);
-
-  }
-
-  function HandleConfirmDeleteUser(id: string) {
-    openModal("Tem certeza que deseja excluir?",
-      <DeleteOptions>
-        <Button variant="destructive" onClick={() => HandleDeleteUser(id)}>Sim</Button>
-        <Button variant="secondary" onClick={hideModal} >Não</Button>
-      </DeleteOptions>
-    )
-  }
-
-  async function HandleDeleteUser(id: string) {
-    try {
-      const response = await api.delete(`/users/delete-user/${id}`)
-
-      toast.success(response.data.message)
-
-      queryClient.invalidateQueries({
-        queryKey: ["users"],
-        exact: false
-      })
-
-      hideModal()
-    } catch (error: any) {
-      toast.error(error.response.data.message)
-    }
-  }
-
-
-
 
   return (
     <>
       <AuthBar />
-      <UsersContent>
-        <UsersHeader>
-          <h2>Users</h2>
-          <Button variant="primary" onClick={() => {
-            openModal("New User",
-              <UserForm />
-            )
-          }}>
-            New User
-          </Button>
-        </UsersHeader>
-        <UsersManagerContent>
-          <UsersTable>
-            <thead>
-              <tr>
-                <th>Identifier</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {isLoading && <UsersSkeleton />}
-              {data?.users.map((user, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{user.id}</td>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                    <td>
-                      <Button variant="destructive" onClick={() => HandleConfirmDeleteUser(user.id)}>Excluir</Button>
-                    </td>
-                  </tr>
-                )
-              }) }
-            </tbody>
-          </UsersTable>
-          {data?.meta && <Pagination
-            onPageChange={HandlePaginate}
-            pageIndex={pageIndex}
-            totalCount={data.meta.totalCount}
-            perPage={data.meta.perPage}
-          />}
-        </UsersManagerContent>
-      </UsersContent>
+      <Suspense>
+        <UsersPanel />
+      </Suspense>
     </>
   )
 }
